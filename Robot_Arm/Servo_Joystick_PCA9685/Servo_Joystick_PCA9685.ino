81b46fb6eec0f46793b44bdd541f403f145fb455
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-07-31 02:22:50 +0200

    Change the programm to use the class Joint

--- Robot_Arm/Servo_Joystick_PCA9685/Servo_Joystick_PCA9685.ino
+++ Robot_Arm/Servo_Joystick_PCA9685/Servo_Joystick_PCA9685.ino
@@ -3,108 +3,93 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define BASE        0
-#define SHOULDER    1
-#define ELBOW       2
-#define WRIST_ROT   3
-#define WRIST       4
-#define GRIPPER     5
-
-#define BAS_MAX     2400
-#define BAS_MIN     450
-
-#define SHL_MAX     2000
-#define SHL_MIN     750
-
-#define ELB_MAX     2050     
-#define ELB_MIN     630
-
-#define WROT_MAX    2150
-#define WROT_MIN    630
-
-#define WRI_MAX     2400
-#define WRI_MIN     450
-
-#define GRI_MAX     2150
-#define GRI_MIN     630
-
 class Joint {
-    private:
-        byte pinNum;
-        unsigned maxVal;
-        unsigned minVal;
-        unsigned pos;
-        unsigned steps;
-    public:
-        Joint();
-        Joint(byte pinNum, unsigned maxVal, unsigned minVal, unsigned pos, unsigned startPos){
-            this->pinNum = pinNum;
-            this->maxVal = maxVal;
-            this->minVal = minVal;
-            this->pos = pos;
-            this->steps = startPos;
-        }
+private:
+  byte pinNum;
+  unsigned maxVal;
+  unsigned minVal;
+  unsigned pos;
+  unsigned steps;
+public:
+  Joint();
+  Joint(byte pinNum, unsigned minVal, unsigned maxVal, unsigned pos, unsigned startPos) {
+    this->pinNum = pinNum;
+    this->maxVal = maxVal;
+    this->minVal = minVal;
+    this->pos = pos;
+    this->steps = startPos;
+  }
 
-        void moveMs (unsigned time){
-            pwm.writeMicroseconds(this->pinNum, time);        
-        }
+  void move() {
+    pwm.writeMicroseconds(this->pinNum, this->pos);
+  }
 
-        void moveDegree (int angle){
-            this->pos = map (angle, maxVal, minVal, 0, 180);
-            moveMs(this->pos);
-        }
+  void moveMs(unsigned time) {
+    this->pos = time;
+    pwm.writeMicroseconds(this->pinNum, this->pos);
+  }
 
-        void moveSteps (bool direction){
-            if (direction == HIGH && (this->pos < this->maxVal)){
-                moveMs((this->pos + this->steps));
-            }
-            else if (direction == HIGH && (this->pos > this->minVal)){
-                moveMs((this->pos - this->steps));
-            }
-            else {}
-        }
+  void moveDegree(int angle) {
+    this->pos = map(angle, maxVal, minVal, 0, 180);
+    moveMs(this->pos);
+  }
+
+  void moveSteps(bool direction) {
+    if (direction == HIGH && (this->pos < this->maxVal)) {
+      this->pos += this->steps;
+    } else if (direction == LOW && (this->pos > this->minVal)) {
+      this->pos -= this->steps;
+    }
+    move();
+  }
+
+  unsigned position() {
+    return pos;
+  }
 };
 
 
-int pos1 = 1500;
-int pos2 = 1390;
-int pos3 = 1300;
-int pos4 = 1300;
-int pos5 = 1300;
-int dt=10;
+int dt = 10;
 int steps = 5;
+
+Joint base(0, 450, 2400, 1500, steps);
+Joint shoulder(1, 750, 2000, 1390, (steps / 2));
+Joint elbow(2, 630, 2050, 1300, steps);
+Joint wrist(3, 630, 2150, 1300, (steps / 2));
+Joint wristRot(4, 450, 2400, 1300, steps);
+Joint gripper(5, 630, 2150, 1300, steps);
 
 void setup() {
   Serial.begin(115200);
-  
+
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
-  
-  pinMode(A0,INPUT);      // Joystick 1 X
-  pinMode(A1,INPUT);      // Joystick 1 Y
-  pinMode(A2,INPUT);      // Joystick 2 X
-  pinMode(A3,INPUT);      // Joystick 2 Y
-  pinMode(7, OUTPUT);     // Relay
-  
-  pinMode (2,INPUT);
-  pinMode (8,INPUT);
+
+  pinMode(A0, INPUT);  // Joystick 1 X
+  pinMode(A1, INPUT);  // Joystick 1 Y
+  pinMode(A2, INPUT);  // Joystick 2 X
+  pinMode(A3, INPUT);  // Joystick 2 Y
+  pinMode(7, OUTPUT);  // Relay
+
+  pinMode(2, INPUT);
+  pinMode(8, INPUT);
 
   digitalWrite(7, LOW);
 
-  digitalWrite(2, HIGH);  
+  digitalWrite(2, HIGH);
   digitalWrite(8, HIGH);
 
   delay(10);
-  
-  pwm.writeMicroseconds(BASE, pos1);        
-  pwm.writeMicroseconds(SHOULDER, pos2);      
-  pwm.writeMicroseconds(ELBOW, pos3);      
-  pwm.writeMicroseconds(WRIST_ROT, pos4);      
-  pwm.writeMicroseconds(WRIST, pos5);  
+
+  base.move();
+  shoulder.move();
+  elbow.move();
+  wrist.move();
+  wristRot.move();
+  gripper.move();
 
   delay(200);
-  
 }
 
 void loop() {
@@ -115,97 +100,43 @@ void loop() {
 
   //Serial.println(digitalRead(2));
   //delay(200);
-  
-  if (y1_val > 800) {
-    if (pos1 < BAS_MAX) {
-      pwm.writeMicroseconds(BASE, pos1);  
-      pos1+=steps;
-      //Serial.println(pos1);
-      delay(dt);
-    }
 
+  if (y1_val > 800) {
+    base.moveSteps(HIGH);
   }
   if (y1_val < 400) {
-    if (pos1 > BAS_MIN) {
-      pwm.writeMicroseconds(BASE, pos1);  
-      pos1-=steps;
-      //Serial.println(pos1);
-      delay(dt);
-    }
+    base.moveSteps(LOW);
   }
 
-  
-  if (x1_val > 800) {
-    if (pos2 < SHL_MAX) {
-      pwm.writeMicroseconds(SHOULDER, pos2);  
-      pos2+= (steps/10);
-      //Serial.println(pos2);
-      delay(dt);
-    }
 
+  if (x1_val > 800) {
+    shoulder.moveSteps(HIGH);
   }
   if (x1_val < 400) {
-    if (pos2 > SHL_MIN) {
-      pwm.writeMicroseconds(SHOULDER, pos2);  
-      pos2+= (steps/10);
-      //Serial.println(pos2);
-      delay(dt);
-    }
+    shoulder.moveSteps(LOW);
   }
-
 
   if (x2_val > 800) {
-    if (pos3 < ELB_MAX) {
-      pwm.writeMicroseconds(ELBOW, pos3);  
-      pos3+=steps;
-      //Serial.println(pos3);
-      delay(dt);
-    }
+    elbow.moveSteps(HIGH);
   }
   if (x2_val < 400) {
-    if (pos3 > ELB_MIN) {
-      pwm.writeMicroseconds(ELBOW, pos3);  
-      pos3-=steps;
-      //Serial.println(pos3);
-      delay(dt);
-    }
+    elbow.moveSteps(LOW);
   }
 
-  
-  if (y2_val > 800) {
-    if (pos4 < WROT_MAX) {
-      pwm.writeMicroseconds(WRIST_ROT, pos4);  
-      pos4+=steps;
-      //Serial.println(pos4);
-      delay(dt);
-    }
 
+  if (y2_val > 800) {
+    wrist.moveSteps(HIGH);
   }
   if (y2_val < 400) {
-    if (pos4 > WROT_MIN) {
-      pwm.writeMicroseconds(WRIST_ROT, pos4);  
-      pos4-=steps;
-      //Serial.println(pos4);
-      delay(dt);
-    }
+    wrist.moveSteps(LOW);
   }
 
 
-  if (digitalRead(2) == HIGH && digitalRead(8)== LOW) {
-    if (pos5 < WRI_MAX) {
-      pwm.writeMicroseconds(WRIST, pos5);  
-      pos5+=steps;
-      //Serial.println(pos5);
-      delay(dt);
-    }
+  if (digitalRead(2) == HIGH && digitalRead(8) == LOW) {
+    wristRot.moveSteps(HIGH);
   }
-  
-  if (digitalRead(8) == HIGH && digitalRead(2)== LOW) {
-    if (pos5 > WRI_MIN) {
-      pwm.writeMicroseconds(WRIST, pos5);  
-      pos5-=steps;
-      //Serial.println(pos5);
-      delay(dt);
-    }
+  if (digitalRead(2) == LOW && digitalRead(8) == HIGH) {
+    wristRot.moveSteps(LOW);
   }
+  delay(dt);
 }
