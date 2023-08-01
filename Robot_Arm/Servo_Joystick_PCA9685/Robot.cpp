c03bdd53ee68148a001792148de3b60d922b1b3c
Yaman Alsaady <alsaady.yaman@gmail.com>
2023-08-01 22:33:13 +0200

    Creating a file for all the functions that controlls the Robot

---
+++ Robot_Arm/Servo_Joystick_PCA9685/Robot.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include "Robot.hh"
+#include "Joint.hh"
+
+Robot::Robot(joint *base, joint *shoulder, joint *elbow, joint *wrist, joint *wristRot, joint *gripper) {
+  this->base = base;
+  this->shoulder = shoulder;
+  this->elbow = elbow;
+  this->wrist = wrist;
+  this->wristRot = wristRot;
+}
+
+Robot::moveWithJoystick(byte x1Pin, byte y1Pin, byte button1Pin, byte x2Pin, byte y2Pin, byte button2Pin) {
+  if (analogRead(y1Pin) > 800) {
+    this->base->moveSteps(HIGH);
+  }
+  if (analogRead(y1Pin) < 400) {
+    this->base->moveSteps(LOW);
+  }
+
+
+  if (analogRead(x1Pin) > 800) {
+    this->shoulder->moveSteps(HIGH);
+  }
+  if (analogRead(x1Pin) < 400) {
+    this->shoulder->moveSteps(LOW);
+  }
+
+  if (analogRead(x2Pin) > 800) {
+    this->elbow->moveSteps(HIGH);
+  }
+  if (analogRead(x2Pin) < 400) {
+    this->elbow->moveSteps(LOW);
+  }
+
+
+  if (analogRead(y2Pin) > 800) {
+    this->wrist->moveSteps(HIGH);
+  }
+  if (analogRead(y2Pin) < 400) {
+    this->wrist->moveSteps(LOW);
+  }
+
+
+  if (digitalRead(button1Pin) == HIGH && digitalRead(button2Pin) == LOW) {
+    this->wristRot->moveSteps(HIGH);
+  }
+  if (digitalRead(button1Pin) == LOW && digitalRead(button2Pin) == HIGH) {
+    this->wristRot->moveSteps(LOW);
+  }
+}
--- Robot_Arm/Servo_Joystick_PCA9685/Robot.hh
+++ Robot_Arm/Servo_Joystick_PCA9685/Robot.hh
@@ -1,22 +1,27 @@
 #ifndef ROBOT_H
+#define ROBOT_H
 #include "Joint.hh"
 #include <Arduino.h>
 
 class Robot {
 private:
-  joint base;
-  joint shoulder;
-  joint elbow;
-  joint wrist;
-  joint wristRot;
-  joint gripper;
+  joint *base;
+  joint *shoulder;
+  joint *elbow;
+  joint *wrist;
+  joint *wristRot;
+  joint *gripper;
   int xPos;
   int yPos;
   int zPos;
 
 public:
-  Robot(joint base, joint shoulder, joint elbow, joint wrist, joint wristRot,
-        joint gripper);
+  //Robot();
+  Robot(joint *base, joint *shoulder, joint *elbow, joint *wrist, joint *wristRot, joint *gripper);
+
+  //moveEndEfffector(xVal, yVal, zVal, RotionDegree);
+  //moveJoints();
+  moveWithJoystick(byte x1Pin, byte y1Pin, byte button1Pin, byte x2Pin, byte y2Pin, byte button2Pin);
 };
 
-#endif // !ROBOT_H
+#endif  // !ROBOT_H
